21be00c9e32042288d04e76646fafbcaa9adbe2c
Kenzy2112 <123618123+kenzy51@users.noreply.github.com>
2024-01-26 15:24:17 +0600

    changed product model and controller

--- routes/product/product.model.js
+++ routes/product/product.model.js
@@ -1,28 +1,18 @@
 //Load Packages
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require("mongoose");
 
-var ProductSchema = new Schema({
-	name: String,
-	categories: { type: [Schema.ObjectId], ref: 'Category' }, 
-  description: String,
-  price: Number,
-  specialPrice: Number,
-  photos: [String],
-  popular: Boolean,
-  quantity: Number,
-  visible: { type: Boolean, default: true },
-  location: String,
-	locationPoints: {
-		type: { type: String, enum: ['Point'] },
-		coordinates: { type: [Number] }
-	},
-  additionalDetails: [String]
-}, {
-  timestamps: true
-});
+var ProductSchema = new Schema(
+  {
+    name: String,
+    photos: [String],
+    description: String,
+    location: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-ProductSchema.index({ "locationPoints": "2dsphere" });
+const Product = mongoose.model("Product", ProductSchema);
 
-//return the model
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = Product;
